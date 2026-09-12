5b5fed6c8c8bb94b56866514c0d97320c3125c74
glacerous <azzakyraihan@gmail.com>
2026-09-12 12:24:20 +0700

    refactor: move measure_recalculate_mem.py to tests/scratch_eval/

--- tests/scratch_eval/measure_recalculate_mem.py
+++ tests/scratch_eval/measure_recalculate_mem.py
@@ -6,7 +6,7 @@ import subprocess
 import requests
 import numpy as np
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from dotenv import load_dotenv
 load_dotenv()
 
